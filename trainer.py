19c14a6f2dc5eb0f3e1f9276940d6621016a760a
torquecoder <torquecode@gmail.com>
2017-06-09 17:12:02 +0530

    code added for making a convolutional layer

--- trainer.py
+++ trainer.py
@@ -16,7 +16,7 @@ num_filters_1 = 64
 # Fully connected layer
 fc_size = 128   # Number of neurons in fully connected layer
 
-# Number of color channels in image
+# Number of color channels in an image
 num_channels = 3    #RGB
 
 # Image dimensions
@@ -58,4 +58,59 @@ def new_weights(shape):
     return tf.Variable(tf.truncated_normal(shape, stddev = 0.05)) # Standard Deviation = 0.05
 
 def new_biases(length):
-    return tf.Variable(tf.constant(0.05, shape=[length]))
+    return tf.Variable(tf.constant(0.05, shape = [length]))
+
+
+def new_conv_layer(input,              # The previous layer
+               num_input_channels,     # Number of channels in previous layer
+               filter_size,            # Width and height of each filter
+               num_filters,            # Number of filters
+               use_pooling = True):    # Use 2x2 max-pooling
+
+    # Shape of the filter-weights for the convolution
+    # This format is determined by the TensorFlow API
+    shape = [filter_size, filter_size, num_input_channels, num_filters]
+
+    # Create new weights aka. filters with the given shape
+    weights = new_weights(shape = shape)
+
+    # Create new biases, one for each filter
+    biases = new_biases(length = num_filters)
+
+    # Create the TensorFlow operation for convolution
+    # Note the strides are set to 1 in all dimensions
+    # The first and last stride must always be 1,
+    # because the first is for the image-number and
+    # the last is for the input-channel
+    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
+    # is moved 2 pixels across the x- and y-axis of the image
+    # The padding is set to 'SAME' which means the input image
+    # is padded with zeroes so the size of the output is the same
+    layer = tf.nn.conv2d(input = input, filter = weights, strides=[1, 1, 1, 1], padding = 'SAME')
+
+    # Add the biases to the results of the convolution
+    # A bias-value is added to each filter-channel
+    layer += biases
+
+    # Use pooling to down-sample the image resolution
+    if use_pooling:
+        # This is 2x2 max-pooling, which means that we
+        # consider 2x2 windows and select the largest value
+        # in each window. Then we move 2 pixels to the next window.
+        layer = tf.nn.max_pool(value=layer,
+                               ksize = [1, 2, 2, 1],       # Size of max-pooling window (2x2)
+                               strides = [1, 2, 2, 1],     # stride on a single image (2x2)
+                               padding = 'SAME')
+
+    # Rectified Linear Unit (ReLU)  (Some alien name as said by Siraj Raval :P)
+    # It calculates max(x, 0) for each input pixel x
+    # This adds some non-linearity to the formula and allows us to learn more complicated functions
+    layer = tf.nn.relu(layer)
+
+    # Note that ReLU is normally executed before the pooling,
+    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+    # save 75% of the relu-operations by max-pooling first.
+
+    # We return both the resulting layer and the filter-weights
+    # because we will plot the weights later.
+    return layer, weights
